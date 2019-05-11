31463effd8b756b20ed3a43bcf8c92aa9ef97cfb
Pablo González Doval <pablo.gonzalez@masmovil.com>
2019-05-11 12:43:55 +0200

    Tested that the algorithm is non-blocking
    Imported vertx for testing only, and ran the tests
    inside a verticle. Increased size to 50k, so that it's
    easier to detect blocked threads.

--- flowables-composition/src/test/java/com/dovaleac/flowablesComposition/strategy/instance/DepleteLeftJoinStrategyInstanceTest.java
+++ flowables-composition/src/test/java/com/dovaleac/flowablesComposition/strategy/instance/DepleteLeftJoinStrategyInstanceTest.java
@@ -1,9 +1,7 @@
 package com.dovaleac.flowablesComposition.strategy.instance;
 
 import com.dovaleac.flowablesComposition.PlannerConfig;
-import com.dovaleac.flowablesComposition.scenario.Scenario;
 import io.reactivex.BackpressureStrategy;
-import io.reactivex.functions.Function;
 
 import java.nio.file.Path;
 import java.util.HashMap;
